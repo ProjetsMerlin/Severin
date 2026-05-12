1e56af852b2b3bde1e35e56e8a17e4115948911a
unknown <infolintermediaire@gmail.com>
2026-05-12 14:37:49 +0200

    repair link

--- index.php
+++ index.php
@@ -13,60 +13,44 @@ $config = $data['config'];
 
 ?>
 <!DOCTYPE html>
-
 <html lang="<?= $config['lang'] ?>">
 
 <head>
-
     <meta charset="<?= $config['charset'] ?>">
-
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title><?= htmlspecialchars($config['siteName']) ?> - <?= htmlspecialchars($page['seo']['title']) ?></title>
 
     <meta name="description" content="<?= htmlspecialchars($page['seo']['description']) ?>">
-
     <meta name="author" content="<?= htmlspecialchars($config['author']) ?>">
 
     <base href="<?= htmlspecialchars($config['siteUrl']) ?>">
 
     <link rel="icon" href="<?= htmlspecialchars($config['favicon']) ?>">
-
     <link rel="stylesheet" href="assets/style.css">
 </head>
 
 <body class="severin">
-
     <?php
-
-    $Menu = "Composants/Menu/index.php";
-        require_once $Menu;
-        $function = 'renderMenu';
-            $function($config['menu']);
+        require_once "Composants/Menu/index.php";
+        renderMenu($config['menu']);
 
 foreach ($page['components'] as $section) {
-
     $componentName = $section['component'];
-
     $componentPath = "Composants/$componentName/index.php";
 
     if (file_exists($componentPath)) {
-
         require_once $componentPath;
-
         $function = 'render' . $componentName;
 
         if (function_exists($function)) {
-
             $function($section['data']);
         }
     }
 }
-
 ?>
 
     <script type="module" src="assets/app.js"></script>
 
 </body>
-
 </html>
